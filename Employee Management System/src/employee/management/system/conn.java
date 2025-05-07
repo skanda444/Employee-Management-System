f3edd72cb602941decb3de16e3d625cf07c35746
skanda444 <skandachandir@gmail.com>
2025-05-07 15:35:39 +0530

    Add files via upload
    Uploaded the initial version of the Employee Management System project.
    Includes database schema, SQL queries, and project files related to employee data handling.
    Features CRUD operations, department-wise management, and salary tracking functionalities.

---
+++ Employee Management System/src/employee/management/system/conn.java
@@ -0,0 +1,17 @@
+package employee.management.system;
+import java.sql.*;
+public class conn {
+    
+    Connection c;
+    Statement s;
+    
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem","root","Qazwsx1@1");
+            s=c.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
